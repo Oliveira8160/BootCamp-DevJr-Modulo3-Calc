d02465cb06ab2512a1d54856cb77b50d7fcf0810
oliveira8160 <gabriel.0812.oliveira@gmail.com>
2024-02-28 06:50:25 -0300

    closes #4 - 4 operaçãos adicionadas

--- javascript.js
+++ javascript.js
@@ -1,7 +1,15 @@
-function calc(){
+function calc(op){
     var number1 = parseFloat(document.getElementById("number1").value);
     var number2 = parseFloat(document.getElementById("number2").value);
+    var result = 0;
 
-    var result = number1 + number2;
+    switch(op)
+    {
+        case '+' : result = number1 + number2; break;
+        case '-' : result = number1 - number2; break;
+        case '*' : result = number1 * number2; break;
+        case '/' : result = number1 / number2; break;
+    }
+
     document.getElementById("result").value = result;
 }
